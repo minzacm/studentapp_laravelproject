f56c32b3fcd1cb6dffe7146ee844916aa54457b9
minzacm <minzacn@gmail.com>
2021-03-17 12:47:45 +0530

    msg

--- resources/views/faculty.blade.php
+++ resources/views/faculty.blade.php
@@ -29,14 +29,30 @@
     </div>
   </div>
 </nav>
-<div class="conatiner">
+<div class="container">
 <div class="row">
 <div class="col col-12 col-sm-4 col-mid-4 col-lg-4 col-xl-4 col-xxl-4"></div>
 
 <div class="col col-12 col-sm-4 col-mid-4 col-lg-4 col-xl-4 col-xxl-4">
-<table class="table table-borderless">
-<tr>
+<table class="table">
+</table><tr>
     <td>Name</td>
+    <td><input type="text" class="form-control"></td>
+</tr>
+<tr>
+    <td>Department</td>
+    <td><input type="text" class="form-control"></textarea></td>
+</tr>
+<tr>
+    <td>Address</td>
+    <td><textarea name="" id="" cols="30" rows="10" class="form-control"></textarea></td>
+</tr>
+<tr>
+    <td>Phone Number</td>
+    <td><input type="text" class="form-control"></td>
+</tr>
+<tr>
+    <td>Education Qualification</td>
     <td><input type="text" class="form-control"></td>
 </tr>
 <tr>
@@ -44,23 +60,15 @@
     <td><input type="text" class="form-control"></td>
 </tr>
 <tr>
-    <td>Roll Number</td>
-    <td><input type="text" class="form-control"></td>
-</tr>
-<tr>
-    <td>Admission Number</td>
-    <td><input type="text" class="form-control"></td>
-</tr>
-<tr>
     <td></td>
-    <td><button class="btn btn-success">SUBMIT</button></td>
+    <td><button class="btn btn-success">Submit</button></td>
 </tr>
-</table>
+
 
 </div>
-<div class="col col-12 col-sm-4 col-mid-4 col-lg-4 col-xl-4 col-xxl-4">
-</div>
+<div class="col col-12 col-sm-4 col-mid-4 col-lg-4 col-xl-4 col-xxl-4"></div>
+    
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
-
+   
 </body>
 </html>
